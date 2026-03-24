3d94db5ef9c2b9aaec00f32c99f17e536a030e17
Matt Hunzinger <matt@hunzinger.me>
2026-03-23 21:37:27 -0400

    feat: initial commit

---
+++ Circuitlib.lean
@@ -0,0 +1,7 @@
+/-
+Copyright (c) 2026 Matt Hunzinger. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Matt Hunzinger
+-/
+
+import Circuitlib.Basic
