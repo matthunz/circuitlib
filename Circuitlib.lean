2580c7f6237ed4190569ebd7c8db814923edbd99
Matt Hunzinger <matt@hunzinger.me>
2026-03-23 22:29:34 -0400

    feat: Gate

--- Circuitlib.lean
+++ Circuitlib.lean
@@ -4,4 +4,4 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Matt Hunzinger
 -/
 
-import Circuitlib.Circuit.Wires
+import Circuitlib.Circuit.Gate
---
+++ Circuitlib/Circuit/Gate.lean
@@ -0,0 +1,32 @@
+/-
+Copyright (c) 2026 Matt Hunzinger. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Matt Hunzinger
+-/
+
+module
+
+public import Circuitlib.Circuit.Wires
+public import Mathlib.Tactic.TypeStar
+public import Mathlib.Order.Monotone.Defs
+
+@[expose] public section
+
+/-! # Gates
+
+## References
+
+* [Ghica, Kaye, and Sprunger, *A Complete Theory of Sequential Digital Circuits*][Ghica2025]
+
+-/
+
+namespace Circuit
+
+/-- Logic gate. -/
+class Gate (V : outParam Type*) [Preorder V] (G : Type*) where
+  inputs : G → ℕ
+  outputs : G → ℕ
+  gate (g : G) : Wires V (inputs g) → Wires V (outputs g)
+  gate_monotone (g : G) : Monotone (gate g)
+
+end Circuit
